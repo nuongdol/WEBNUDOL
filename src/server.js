a80b03ec6aba148e96299235c16ce4aaffa0fc66
nuong dang <dangthinuong1804@gmail.com>
2022-10-05 11:42:44 +0700

    view engine

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 import express from 'express';
-//const express = require('express')
+import configViewEngine from './configs/viewEngine';
 
 const app = express()
+//const express = require('express')
+//const path = require('path');
+configViewEngine(app);
+
 const port = 8080
 
 app.get('/', (req, res) => {
-    res.send('Hello World! vs Hoi Dan IT and Eric')
+    res.render('test/index.ejs')
 })
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './index.html'));
+// })
+
 app.get('/about', (req, res) => {
-    res.send(`I'm Eric`)
+    res.send(`I'm Nuong and i am a student!!`)
 })
 
 app.listen(port, () => {
